386d3225536e0fbcdd70e7b75f40abd5f87d877a
Aleksei <avroshchin@gmail.com>
2023-08-13 12:48:38 +0300

    Initial Commit

---
+++ RandomTeam/RandomTeamApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomTeamApp.swift
+//  RandomTeam
+//
+//  Created by Алексей Рощин on 13.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct RandomTeamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
